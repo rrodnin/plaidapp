2d58603bf26efd8ad131a7304733e769932ef4aa
Rodnin Roman <rodninroma@gmail.com>
2018-11-21 22:04:31 +0300

    inital commit

---
+++ src/main/java/com/aradata/plaidapp/PlaidappApplication.java
@@ -0,0 +1,12 @@
+package com.aradata.plaidapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaidappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaidappApplication.class, args);
+	}
+}
