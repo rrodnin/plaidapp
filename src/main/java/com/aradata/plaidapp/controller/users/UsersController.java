0720851702f8e99034e961c790ae5c175ebc0211
Rodnin Roman <rodninroma@gmail.com>
2018-11-26 10:44:53 +0300

    HATEOAS response on users/news

--- src/main/java/com/aradata/plaidapp/controller/users/UsersController.java
+++ src/main/java/com/aradata/plaidapp/controller/users/UsersController.java
@@ -1,15 +1,23 @@
 package com.aradata.plaidapp.controller.users;
 
+import com.aradata.plaidapp.controller.content.ContentController;
 import com.aradata.plaidapp.model.content.AppConstants;
+import com.aradata.plaidapp.model.content.response.ContentResponse;
+import com.aradata.plaidapp.model.payloads.PagedResponse;
 import com.aradata.plaidapp.security.CurrentUser;
 import com.aradata.plaidapp.security.UserPrincipal;
 import com.aradata.plaidapp.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/api/users")
@@ -22,6 +30,34 @@ public class UsersController {
 	public ResponseEntity<?> getNews(@CurrentUser UserPrincipal currentUser,
 	                                 @RequestParam(value = "page", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER) int page,
 	                                 @RequestParam(value = "size", defaultValue = AppConstants.DEFAULT_PAGE_SIZE) int size) {
-		return ResponseEntity.ok(usersService.getUserNews(currentUser, page, size));
+		PagedResponse<ContentResponse> response = usersService.getUserNews(currentUser, page, size);
+		response.getContent().stream()
+				.forEach(contentResponse -> {
+					contentResponse.add(
+							linkTo(methodOn(ContentController.class).getContentById(
+									currentUser,
+									contentResponse.getContentId()
+							)).withSelfRel());
+					contentResponse.add(
+							linkTo(methodOn(ContentController.class).getComments(
+									currentUser,contentResponse.getContentId(),0,30
+							)).withRel("comments"));
+					contentResponse.add(
+							linkTo(methodOn(ContentController.class).createLike(
+									contentResponse.getContentId(), currentUser
+							)).withRel("likes"));
+				});
+
+		Link link = linkTo(methodOn(ContentController.class).getContents(currentUser, page, size)).withSelfRel();
+		Link linkToNext = linkTo(methodOn(ContentController.class).getContents(currentUser, page, size)).withRel("next");
+		Link linkToPrev = linkTo(methodOn(ContentController.class).getContents(currentUser, page, size)).withRel("prev");
+		if (page < response.getTotalPages() - 1) {
+			linkToNext = linkTo(methodOn(ContentController.class).getContents(currentUser, page+1, size)).withRel("next");
+		}
+		if (page > 0) {
+			linkToPrev = linkTo(methodOn(ContentController.class).getContents(currentUser, page-1, size)).withRel("prev");
+		}
+		Resource<PagedResponse<ContentResponse>> responseResource = new Resource<>(response, link, linkToNext, linkToPrev);
+		return ResponseEntity.ok().body(responseResource);
 	}
 }
